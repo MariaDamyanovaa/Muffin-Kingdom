6fe951b6cce3cd7eb57b4c5be4de7d8f19c3af10
Maria Damyanova <mariadamyanova1@gmail.com>
2025-02-27 11:16:04 +0200

    farst commit

---
+++ MVC-Muffin-Kingdon/Data/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MVC_Muffin_Kingdon.Data
+{
+    public class User:IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+}
