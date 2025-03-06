0fdb3f4c0a1d1332d8c87e970cef5f58bb42e92d
Maria Damyanova <mariadamyanova1@gmail.com>
2025-03-06 11:41:37 +0200

    controllers+register+login

--- MVC-Muffin-Kingdon/Program.cs
+++ MVC-Muffin-Kingdon/Program.cs
@@ -1,22 +1,41 @@
+﻿using ASPShopBag.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using MVC_Muffin_Kingdon.Areas.Identity.Pages.Account;
 using MVC_Muffin_Kingdon.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+// Добавяне на Identity услуги
+builder.Services.AddDefaultIdentity<User>(options =>
+                                options.SignIn.RequireConfirmedAccount = false)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+
+// Добавяне на допълнителни услуги и конфигурации
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//IMvcBuilder buildMVC = builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddControllers(
+    options =>
+    options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+//if (builder.Environment.IsDevelopment())
+//{
+//    builder.AddRazorRuntimeCompilation();
+//}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Конфигурация на мидълуерите и HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -24,20 +43,24 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.PrepareDataBase().Wait();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
+
+
+
